ddeed56c462516842ba59aabc01cce98f7e5c1d8
Williamcs1400 <williamcs1400@gmail.com>
2021-04-24 16:31:19 -0300

    Forest encaminhado

--- randomForest.py
+++ randomForest.py
@@ -9,13 +9,43 @@
 
 #Importando bibliotecas necessarias
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 #Le a base de dados
-
 dataset = pd.read_excel('dataset.xlsx', engine='openpyxl')
 
-dataset.head()
-dataset.info()
+#Converter dados
+labels = np.array(dataset['resultado do exame'])
+datasetList = list(dataset.columns)
+dataset = dataset.drop('resultado do exame', axis=1)
+dataset = np.array(dataset)
+
+print(dataset)
+print(labels)
+
+#Treinamento dos dados
+trainDataset, testDataset, trainLabels, testLabels = train_test_split(dataset, labels, test_size=0.25, random_state=42)
+
+print ('Training Features Shape:', trainDataset.shape) 
+print ('Training Labels Shape:', testDataset.shape) 
+print ('Testing Features Shape:', trainLabels.shape) 
+print ('Testing Labels Shape:', testLabels.shape)
+
+#Deixando resultado testLabels binário
+for i, x in enumerate(testLabels):
+    if(x == 'negative'):
+        testLabels[i] = 0
+    else:
+        testLabels[i] = 1
+
+
+#Previsoes
+baselinePreds = testDataset[:, datasetList.index('resultado do exame')]
+baselineErrors = abs(baselinePreds - testLabels)
+
+print(baselinePreds)
+print('Average baseline error: ', round(np.mean(baselineErrors), 2))
 
 
 
