c7b9ceed369f7d73f99fe35b6f927fc3cd7b8d96
Williamcs1400 <williamcs1400@gmail.com>
2021-05-01 20:54:30 -0300

    Add decision tree

---
+++ decisonTree.py
@@ -0,0 +1,16 @@
+#Universidade de Brasilia - UnB
+#Introducao a inteligencia artificial
+#Professor: Dibio Leandro Borges
+#Aluno: William Coelho da Silva - 180029274
+
+#Necessario executar:
+#pip install xlrd 
+#pip install openpyxl
+
+#Importando bibliotecas necessarias
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+#%matplotlib inline
+
+dataset = pd.read_excel('dataset.xlsx', engine='openpyxl')
